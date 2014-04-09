d304ce895749738adb9a22af3cb42387a8de3de9
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-04-09 15:47:07 +0900

    make Event.toString smart

--- src/main/java/net/kaoriya/qb/words_finder/Event.java
+++ src/main/java/net/kaoriya/qb/words_finder/Event.java
@@ -20,12 +20,12 @@ class Event {
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
-        s.append("Event {")
-            .append(" id=").append(this.id)
+        s.append("Event{")
+            .append("id=").append(this.id)
             .append(" nextId=").append(this.nextId)
             .append(" index=").append(this.index)
             .append(" last=").append(this.last)
-            .append(" }");
+            .append("}");
         return s.toString();
     }
 }
